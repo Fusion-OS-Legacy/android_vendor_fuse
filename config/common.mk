aosp-star/platform_vendor_starlight

vendor: Add backuptools
Change-Id: If42aa8b3f97ccded3a9f470e1c7c1b393680a461

config/common.mk
@@ -32,3 +32,15 @@ TARGET_FACE_UNLOCK_SUPPORTED := true
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.face.moto_unlock_service=$(TARGET_FACE_UNLOCK_SUPPORTED)
 
+# Backup Tool
+PRODUCT_COPY_FILES += \
+    vendor/starlight/products/common/bin/backuptool.sh:install/bin/backuptool.sh \
+    vendor/starlight/products/common/bin/backuptool.functions:install/bin/backuptool.functions \
+    vendor/starlight/products/common/bin/50-base.sh:system/addon.d/50-base.sh
+
+ifneq ($(AB_OTA_PARTITIONS),)
+PRODUCT_COPY_FILES += \
+    vendor/starlight/products/common/bin/backuptool_ab.sh:system/bin/backuptool_ab.sh \
+    vendor/starlight/products/common/bin/backuptool_ab.functions:system/bin/backuptool_ab.functions \
+    vendor/starlight/products/common/bin/backuptool_postinstall.sh:system/bin/backuptool_postinstall.sh
+endif

Fusion-OS-Legacy/android_vendor_fuse

Signed-off-by: ItsVixano <vixanonew@gmail.com>
Signed-off-by: SparXFusion <s2234nadar@gmail.com>

config/common.mk
@@ -21,8 +21,21 @@ $(call inherit-product, vendor/fuse/config/versioning.mk)
 $(call inherit-product, vendor/fuse/config/BoardConfigFuse.mk)
 
 # Inherit boot animation
-$(call inherit-product, vendor/aosp/config/bootanimation.mk)
+$(call inherit-product, vendor/fuse/config/bootanimation.mk)
 
 # ThemePicker
 PRODUCT_PACKAGES += \
     ThemePicker
+
+# Backup Tool
+PRODUCT_COPY_FILES += \
+    vendor/fuse/products/common/bin/backuptool.sh:install/bin/backuptool.sh \
+    vendor/fuse/products/common/bin/backuptool.functions:install/bin/backuptool.functions \
+    vendor/fuse/products/common/bin/50-base.sh:system/addon.d/50-base.sh
+
+ifneq ($(AB_OTA_PARTITIONS),)
+PRODUCT_COPY_FILES += \
+    vendor/fuse/products/common/bin/backuptool_ab.sh:system/bin/backuptool_ab.sh \
+    vendor/fuse/products/common/bin/backuptool_ab.functions:system/bin/backuptool_ab.functions \
+    vendor/fuse/products/common/bin/backuptool_postinstall.sh:system/bin/backuptool_postinstall.sh
+endif